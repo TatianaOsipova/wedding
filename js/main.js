9a251c29925510f3b65d5b51c00e93fb1274f7be
Tatiana Osipova <otv120780@gmail.com>
2022-11-12 11:51:44 -0500

    feat: correct carousel

--- js/main.js
+++ js/main.js
@@ -1,48 +1,74 @@
-// let items = document.querySelectorAll('.item');
-// let currentItem = 0;
-// let isEnabled = true;
+// $('.center').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 3,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
 
-// function changeCurrentItem(n) {
-// 	currentItem = (n + items.length) % items.length;
-// }
+let items = document.querySelectorAll('.item');
+let currentItem = 0;
+let isEnabled = true;
 
-// function hideItem(direction) {
-// 	isEnabled = false;
-// 	items[currentItem].classList.add(direction);
-// 	items[currentItem].addEventListener('animationend', function(){
-// 		this.classList.remove('active', direction);
-// 	});
-// }
+function changeCurrentItem(n) {
+	currentItem = (n + items.length) % items.length;
+}
 
-// function showItem(direction) {
-// 	items[currentItem].classList.add('next', direction);
-// 	items[currentItem].addEventListener('animationend', function(){
-// 		this.classList.remove('next', direction);
-// 		this.classList.add('active');
-// 		isEnabled = true;
-// 	});
-// }
+function hideItem(direction) {
+	isEnabled = false;
+	items[currentItem].classList.add(direction);
+	items[currentItem].addEventListener('animationend', function(){
+		this.classList.remove('active', direction);
+	});
+}
 
-// function previousItem(n) {
-// 	hideItem('to-right');
-// 	changeCurrentItem(n - 1);
-// 	showItem('from-left');
-// }
+function showItem(direction) {
+	items[currentItem].classList.add('next', direction);
+	items[currentItem].addEventListener('animationend', function(){
+		this.classList.remove('next', direction);
+		this.classList.add('active');
+		isEnabled = true;
+	});
+}
 
-// function nextItem(n) {
-// 	hideItem('to-left');
-// 	changeCurrentItem(n - 1);
-// 	showItem('from-right');
-// }
+function previousItem(n) {
+	hideItem('to-right');
+	changeCurrentItem(n - 1);
+	showItem('from-left');
+}
 
-// document.querySelector('.control.left').addEventListener('click', function(){
-// 	if (isEnabled) {
-// 		previousItem(currentItem);
-// 	}
-// });
+function nextItem(n) {
+	hideItem('to-left');
+	changeCurrentItem(n - 1);
+	showItem('from-right');
+}
 
-// document.querySelector('.control.right').addEventListener('click', function(){
-// 	if (isEnabled) {
-// 		nextItem(currentItem);
-// 	}
-// });
+document.querySelector('.control.left').addEventListener('click', function(){
+	if (isEnabled) {
+		previousItem(currentItem);
+	}
+});
+
+document.querySelector('.control.right').addEventListener('click', function(){
+	if (isEnabled) {
+		nextItem(currentItem);
+	}
+});
